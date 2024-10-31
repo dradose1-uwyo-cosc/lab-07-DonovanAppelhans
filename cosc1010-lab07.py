d9e20ac0764568e2fa92318131d0b40b9fcdac40
DonovanAppelhans <dappelha@uwyo.edu>
2024-10-31 21:16:16 +0000

    done

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -1,12 +1,12 @@
-# Your Name Here
+# Donovan Appelhans
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
+# 10/31/2024
+# Lab 07
+# Lab Section: 12
 # Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# https://stackoverflow.com/questions/35467303/write-factorial-with-while-loop-python
+# https://teamtreehouse.com/community/how-do-i-write-a-program-that-collects-3-ints-from-a-user-using-a-while-loop-maintain-a-running-total-print-the-sum
+# Google AI
 
 
 # Prompt the user for an upper bound 
@@ -18,6 +18,12 @@
 # You will continue to prompt the user until a proper integer value is entered
 
 factorial = 1
+upper_bound = int(input("Please enter the upper bound: "))
+if upper_bound <= 0:
+    print("Must be a positive number.")
+while upper_bound > 1:
+    factorial = factorial * upper_bound
+    upper_bound = upper_bound - 1
 
 print(f"The result of the factorial based on the given bound is {factorial}")
 
@@ -38,7 +44,11 @@ print("*"*75)
 # The sum should start at 0 
 
 num_sum = 0 
-
+i = 0
+while i < 3:
+    numbers = int(input("Enter any number: "))
+    num_sum += numbers
+    i += 1
 print(f"Your final sum is {num_sum}")
 
 print("*"*75)
@@ -59,4 +69,36 @@ print("*"*75)
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
 
-        
+def calculator():
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
+        return
+
+    operator = input("Enter operator (+, -, *, /, %): ")
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            print("Division by zero is not allowed.")
+            return
+        result = num1 / num2
+    elif operator == "%":
+        result = num1 % num2
+    else:
+        print("Invalid operator. Please enter one of +, -, *, /.")
+        return
+
+    print(f"{num1} {operator} {num2} = {result}")
+
+while True:
+    calculator()
+    if input("Do you want to continue? (y/n): ").lower() != "y":
+        break
